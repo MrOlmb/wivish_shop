3c6dfe8e588fb4ce0686d04647821ae8d379ea42
Elvis Olembe <elvisolembe1@gmail.com>
2025-06-26 13:08:56 +0100

    Fix: Centered the category image in category/new page

--- src/app/dashboard/admin/categories/new/page.tsx
+++ src/app/dashboard/admin/categories/new/page.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import CategoryDetails from "@/components/dashboard/forms/category-details";
 
 export default function AdminNewCategory() {
+  const CLOUDINARY_CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
+  if(!CLOUDINARY_CLOUD_NAME) return null;
   return (
-    <div className="w-full"><CategoryDetails/></div>
+    <div className="w-full">
+      <CategoryDetails cloudinary_key={CLOUDINARY_CLOUD_NAME}/>
+    </div>
   );
 }
--- src/components/dashboard/forms/category-details.tsx
+++ src/components/dashboard/forms/category-details.tsx
@@ -18,13 +18,15 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from "@/components/ui/input"; 
 import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
+import ImageUpload from "../shared/image-upload";
 
 
 interface CategoryDetailsProps{
     data?:Category;
+    cloudinary_key:string;
 }
 
-const CategoryDetails:FC<CategoryDetailsProps> = ({data}) => {
+const CategoryDetails:FC<CategoryDetailsProps> = ({data, cloudinary_key}) => {
     //form hook for managing form state and validation
     const form = useForm<z.infer<typeof CategoryFormSchema>>({
         mode: "onChange", //Form validation mode
@@ -74,14 +76,15 @@ const CategoryDetails:FC<CategoryDetailsProps> = ({data}) => {
             onSubmit={form.handleSubmit(handleSubmit)}
             className="space-y-4"
           >
-            {/* <FormField
+            <FormField
               control={form.control}
               name="image"
               render={({ field }) => (
-                <FormItem>
+                <FormItem className="flex justify-center">
                   <FormControl>
                     <ImageUpload
                       type="profile"
+                      cloudinary_key={cloudinary_key}
                       value={field.value.map((image) => image.url)}
                       disabled={isLoading}
                       onChange={(url) => field.onChange([{ url }])}
@@ -97,7 +100,7 @@ const CategoryDetails:FC<CategoryDetailsProps> = ({data}) => {
                   <FormMessage />
                 </FormItem>
               )}
-            /> */}
+            />
             <FormField
               disabled={isLoading}
               control={form.control}
@@ -163,131 +166,3 @@ const CategoryDetails:FC<CategoryDetailsProps> = ({data}) => {
 }
 
 export default CategoryDetails;
-
-
-
-// "use client";
-
-// // React
-// import { FC, useEffect } from "react";
-
-// // Prisma model
-// import { Category } from "@prisma/client";
-
-// // Form handling utilities
-// import * as z from "zod";
-// import { useForm } from "react-hook-form";
-// import { zodResolver } from "@hookform/resolvers/zod";
-
-// // Schema
-// import { CategoryFormSchema } from "@/lib/schemas";
-
-// // UI Components
-// import { AlertDialog } from "@/components/ui/alert-dialog";
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card";
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-//   FormDescription,
-// } from "@/components/ui/form";
-// import { Checkbox } from "@/components/ui/checkbox";
-// import { Button } from "@/components/ui/button";
-// import { Input } from "@/components/ui/input";
-// import ImageUpload from "../shared/image-upload";
-
-// // Queries
-// import { upsertCategory } from "@/queries/category";
-
-// // Utils
-// import { v4 } from "uuid";
-// import { useToast } from "@/components/ui/use-toast";
-// import { useRouter } from "next/navigation";
-
-// interface CategoryDetailsProps {
-//   data?: Category;
-// }
-
-// const CategoryDetails: FC<CategoryDetailsProps> = ({ data }) => {
-//   // Initializing necessary hooks
-//   const { toast } = useToast(); // Hook for displaying toast messages
-//   const router = useRouter(); // Hook for routing
-
-//   // Form hook for managing form state and validation
-//   const form = useForm<z.infer<typeof CategoryFormSchema>>({
-//     mode: "onChange", // Form validation mode
-//     resolver: zodResolver(CategoryFormSchema), // Resolver for form validation
-//     defaultValues: {
-//       // Setting default form values from data (if available)
-//       name: data?.name,
-//       image: data?.image ? [{ url: data?.image }] : [],
-//       url: data?.url,
-//       featured: data?.featured,
-//     },
-//   });
-
-//   // Loading status based on form submission
-//   const isLoading = form.formState.isSubmitting;
-
-//   // Reset form values when data changes
-//   useEffect(() => {
-//     if (data) {
-//       form.reset({
-//         name: data?.name,
-//         image: [{ url: data?.image }],
-//         url: data?.url,
-//         featured: data?.featured,
-//       });
-//     }
-//   }, [data, form]);
-
-//   // Submit handler for form submission
-//   const handleSubmit = async (values: z.infer<typeof CategoryFormSchema>) => {
-//     try {
-//       // Upserting category data
-//       const response = await upsertCategory({
-//         id: data?.id ? data.id : v4(),
-//         name: values.name,
-//         image: values.image[0].url,
-//         url: values.url,
-//         featured: values.featured,
-//         createdAt: new Date(),
-//         updatedAt: new Date(),
-//       });
-
-//       // Displaying success message
-//       toast({
-//         title: data?.id
-//           ? "Category has been updated."
-//           : `Congratulations! '${response?.name}' is now created.`,
-//       });
-
-//       // Redirect or Refresh data
-//       if (data?.id) {
-//         router.refresh();
-//       } else {
-//         router.push("/dashboard/admin/categories");
-//       }
-//     } catch (error: any) {
-//       // Handling form submission errors
-//       toast({
-//         variant: "destructive",
-//         title: "Oops!",
-//         description: error.toString(),
-//       });
-//     }
-//   };
-
-//   );
-// };
-
-// export default CategoryDetails;
